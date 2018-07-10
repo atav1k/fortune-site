6152378d4b68dd48b5c6426f677be5d5f8a6d34e
Vikram Babu <atav1k@users.noreply.github.com>
2018-07-10 09:24:08 -0500

    scroll eg

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,99 @@
             span Defines gutter space, defaults to
             .code --columns-width
             span Defines column container width, defaults to
-        .col
+        .col.mar-b-4
+          .sticky.top-2
+            h4 Demo
+            .cols.mar-y-1
+              .col-9
+                .bg-lightgray .col-9
+              .col-3
+                .bg-lightgray .col-3
+            .cols.mar-y-1
+              .col
+                .bg-lightgray .col
+              .col
+                .bg-lightgray .col
+            .cols.mar-y-1
+              .col-3
+                .bg-lightgray .col-3
+              .col-9
+                .bg-lightgray .col-9
+            .cols.cols-flush.mar-y-1
+              .col
+                .bg-lightgray .cols-flush .col
+              .col
+                .bg-lightgray .cols-flush .col
+      .cols
+        .col.mar-b-4
+          h3 Columns
+          h5 Classes
+          .grid.grid-cols-2.grid-space-y-1
+            .code .cols
+            span Creates a row of columns
+            .code .cols .cols-flush
+            span Sets columns without gutter
+            .code .col, .col-*
+            span Sets flex or fixed width column
+            .code .offset-*
+            span Offest column by fixed number
+            .code .order-*
+            span Set order of column
+            .code .multi-col-*
+            span Use multi-col css
+          h5.mar-t-2 CSS Properties
+          .grid.grid-cols-2.grid-space-y-1
+            .code --column-gutter
+            span Defines gutter space, defaults to
+            .code --columns-width
+            span Defines column container width, defaults to
+        .col.mar-b-4
+          .sticky.top-2
+            h4 Demo
+            .cols.mar-y-1
+              .col-9
+                .bg-lightgray .col-9
+              .col-3
+                .bg-lightgray .col-3
+            .cols.mar-y-1
+              .col
+                .bg-lightgray .col
+              .col
+                .bg-lightgray .col
+            .cols.mar-y-1
+              .col-3
+                .bg-lightgray .col-3
+              .col-9
+                .bg-lightgray .col-9
+            .cols.cols-flush.mar-y-1
+              .col
+                .bg-lightgray .cols-flush .col
+              .col
+                .bg-lightgray .cols-flush .col
+      .cols
+        .col.mar-b-4
+          h3 Columns
+          h5 Classes
+          .grid.grid-cols-2.grid-space-y-1
+            .code .cols
+            span Creates a row of columns
+            .code .cols .cols-flush
+            span Sets columns without gutter
+            .code .col, .col-*
+            span Sets flex or fixed width column
+            .code .offset-*
+            span Offest column by fixed number
+            .code .order-*
+            span Set order of column
+            .code .multi-col-*
+            span Use multi-col css
+          h5.mar-t-2 CSS Properties
+          .grid.grid-cols-2.grid-space-y-1
+            .code --column-gutter
+            span Defines gutter space, defaults to
+            .code --columns-width
+            span Defines column container width, defaults to
+        .col.mar-b-4
           .sticky.top-2
             h4 Demo
             .cols.mar-y-1
